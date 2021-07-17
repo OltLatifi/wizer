86a3c2d2d8633983848d83c73d34848b35d35276
Olt Latifi <oltlatifi2003@gmail.com>
2021-07-17 19:36:09 +0200

    Added features to the pomodoro, just sound is left

--- wizer-web/src/components/pomodoro/Pomodoro.js
+++ wizer-web/src/components/pomodoro/Pomodoro.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
-// import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -13,8 +11,9 @@ const useStyles = makeStyles({
     root: {
         display: 'flex',
         justifyContent: 'center',
-        flexDirection:'row',
-        margin:'10% 0',
+        flexDirection:'column',
+        alignItems: 'center',
+        marginTop:'10%',
     },
     circle:{
         width: '400px',
@@ -34,10 +33,13 @@ const useStyles = makeStyles({
 function Pomodoro() {
     const classes = useStyles()
     
-    const[minutes, setMinutes] = useState(0);
-    const[seconds, setSeconds] = useState(10);
+    const[minutes, setMinutes] = useState(25);
+    const[seconds, setSeconds] = useState(0);
+
+    const[rest, setRest] = useState(5);
     const[displayMessage, setDisplayMessage] = useState(false);
     const[start, setStart] = useState(false);
+    const[showSettings, setShowSettings] = useState(false);
   
     const timer=()=>{
         let interval = setInterval(() => {
@@ -48,11 +50,11 @@ function Pomodoro() {
                 setSeconds(59);
                 setMinutes(minutes - 1);
                 } else {
-                let minutes = displayMessage ? 24 : 4;
+                let minutes_ = displayMessage ? minutes-1 : rest-1;
                 let seconds = 59;
     
                 setSeconds(seconds);
-                setMinutes(minutes);
+                setMinutes(minutes_);
                 setDisplayMessage(!displayMessage);
                 }
             } else {
@@ -67,25 +69,64 @@ function Pomodoro() {
         }
     }
 
+    const renderSettingsButtons=()=>{
+        if(showSettings){
+            const workButtons = [15, 25, 35]
+            const restButtons = [5, 10, 15]
+            return(
+                <>
+                    <Typography variant="body">Set the work duration: </Typography>
+                    {workButtons.map(button=>{
+                        return(
+                        <Button onClick={()=>{
+                            if(!displayMessage){
+                                setMinutes(button-1)
+                            }
+                            
+                        }}>{button} minutes</Button>
+                        )
+                    })}
+                    <br/>
+                    <Typography variant="body">Set the rest duration: </Typography>
+                    {restButtons.map(button=>{
+                        return(
+                        <Button onClick={()=>{
+                            setRest(button)
+                        }}>{button} minutes</Button>
+                        )
+                    })}
+                </>
+            )
+        }
+    }
+
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
     const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    return (
+    return(
         <>
-        <div className={classes.root}>
-        <div className="message">
-            {displayMessage && <Typography variant="h5">Break time! New session starts in:</Typography>}
-        </div>
-        <div className={classes.circle}>
+            <div className={classes.root}>
+                <div className="message">
+                    {displayMessage && <Typography variant="h5">Break time!🎉 New session starts in:</Typography>}
+                </div>
+                <br/>
+                <div className={classes.circle}>
+                    
+                    <Typography variant="h1">
+                        {timerMinutes}:{timerSeconds}
+                    </Typography>
+                    <Button onClick={()=>setStart(!start)}>{start? 'Stop': 'Start'}</Button>
+                </div>
+                {initiateTimer()}
             
-            <Typography variant="h1">
-                {timerMinutes}:{timerSeconds}
-            </Typography>
-            <Button onClick={()=>setStart(!start)}>{start? 'Stop': 'Start'}</Button>
-        </div>
-        {initiateTimer()}
-        </div>
-        <Button onClick={()=>setStart(!start)}><SettingsIcon/></Button>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', marginTop:'8%'}}>
+                <Button onClick={()=>setShowSettings(!showSettings)} style={{height:40}}><SettingsIcon/></Button>
+                <div style={{marginLeft: '1%', marginTop:'-2%'}}>
+                    {renderSettingsButtons()}
+                </div>
+            </div>
+            
         </>
     );
     }
